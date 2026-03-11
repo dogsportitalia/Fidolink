9676d397199d3f3ed67e3ad636556dbe918d0c52
Andrea D'olivo <andreadolivo@fa1cfaf7-fe49-4028-8bb3-86430245eaae.fritz.box>
2026-03-11 14:01:27 +0100

    Add auto-seed on first startup to restore production data

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 import { startCleanupJob } from "./cleanup";
+import { seedIfEmpty } from "./seed";
 
 const app = express();
 const httpServer = createServer(app);
@@ -61,6 +62,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  seedIfEmpty();
   await registerRoutes(httpServer, app);
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
